ad2359b1b30fb21147f7f6375fdf7bee8e203906
Artemii <shozo.j06@gmail.com>
2025-05-15 09:06:34 +0300

    update

--- src/main/java/io/github/tank_mass_control/controllers/TankController.java
+++ src/main/java/io/github/tank_mass_control/controllers/TankController.java
@@ -7,10 +7,13 @@ import io.github.tank_mass_control.repositories.TankRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -57,4 +60,14 @@ public class TankController {
         result.put("average mass: ", mass);
         return result;
     }
+
+    @GetMapping("/history/filter")
+    public Iterable<TankHistoryModel> getFilteredHistory(@RequestParam Long tankId, @RequestParam String startDate, @RequestParam String endDate) {
+    LocalDateTime start = LocalDateTime.parse(startDate);
+    LocalDateTime end = LocalDateTime.parse(endDate);
+    return ((Collection<TankHistoryModel>) tankHistoryRepository.findAll()).stream()
+            .filter(history -> history.getTank().getId().equals(tankId))
+            .filter(history -> !history.getLocalDateTime().isBefore(start) && !history.getLocalDateTime().isAfter(end))
+            .toList();
+}
 }
